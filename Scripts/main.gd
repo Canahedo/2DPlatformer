7221847cb674d7fac89d2e19cab24be18f909923
Canahedo <Canahedo@gmail.com>
2025-01-09 19:30:25 -0500

    Collectable coin and score

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,19 +1,15 @@
 extends Node2D
 
-@onready var player = $Character
+@onready var player = $Player
 
 
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	Messenger.connect("COIN_COLLECTED", coin_collected)
 	pass # Replace with function body.
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func coin_collected():
-	print("Coin Collected")
--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -1,8 +1,10 @@
-extends Area2D
+extends Control
 
+var score: int = 0
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	Messenger.connect("COIN_COLLECTED", coin_collected)
 	pass # Replace with function body.
 
 
@@ -10,7 +12,7 @@ func _ready():
 func _process(delta):
 	pass
 
-func _on_body_entered(body):
-	if body.name == "Player":
-		Messenger.COIN_COLLECTED.emit()
-		queue_free()
+func coin_collected():
+	print("COIN")
+	score += 1
+	$Score.text = str(score)
